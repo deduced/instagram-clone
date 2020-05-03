aee2320d3fb0e0fbbcf0fdf117049e458fc1c80d
Charlie Astrada <carlosastrada@gmail.com>
2020-05-03 01:22:48 -0300

    Add SEO component to handle the title text (as well as meta, etc if we choose to use it)

--- src/components/shared/Seo.js
+++ src/components/shared/Seo.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Helmet from "react-helmet";
 
-function SEO() {
-  return <div>SEO</div>;
+function SEO({ title }) {
+  const TitleText = title ? `${title} · Instagram` : "Instagram";
+  return (
+    <Helmet>
+      <title>{TitleText}</title>
+    </Helmet>
+  );
 }
 
 export default SEO;
