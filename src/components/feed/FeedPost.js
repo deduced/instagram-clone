9e2923a47a87af98a79b23811b8ce744ab61cec5
Charlie Astrada <carlosastrada@gmail.com>
2020-05-05 00:59:35 -0300

    Add skeleton feed page
    * Sidebar and Feed set up with some dummy data.
    * missing fleshing out user card, like, savebutton, comment custom components.

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import { Link } from "react-router-dom";
+import { MoreIcon, CommentIcon, ShareIcon } from "../../icons";
+import { Typography, Button, Hidden, Divider } from "@material-ui/core";
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const [isShowingCaption, setIsShowingCaption] = useState(false);
+  const { caption, comments, id, likes, media, user } = post;
+  console.log(media);
 
-  return <div>FeedPost</div>;
+  return (
+    <>
+      <article className={classes.article}>
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.moreIcon} />
+        </div>
+        <div>
+          <img src={media} alt="post media" className={classes.image} />
+        </div>
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtonsWrapper}>
+            <LikeButton />
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            <SaveButton />
+          </div>
+          <Typography className={classes.like} variant="subtitle2">
+            <span>{likes === 1 ? `1 like` : `${likes} likes`}</span>
+          </Typography>
+          <div
+            className={isShowingCaption ? classes.expanded : classes.collapsed}
+          >
+            <Link to={`/${user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.username}
+              >
+                {user.username}
+              </Typography>
+            </Link>
+            {isShowingCaption ? (
+              <Typography
+                variant="body2"
+                component="span"
+                dangerouslySetInnerHTML={{ __html: caption }}
+              />
+            ) : (
+              <div className={classes.captionWrapper}>
+                <HTMLEllipsis
+                  unsafeHTML={caption}
+                  className={classes.caption}
+                  maxLine="0"
+                  ellipsis="..."
+                  basedOn="letters"
+                />
+                <Button
+                  className={classes.moreButton}
+                  onClick={() => setIsShowingCaption(true)}
+                >
+                  more
+                </Button>
+              </div>
+            )}
+          </div>
+          <Link to={`/p/${id}`}>
+            <Typography
+              className={classes.commentsLink}
+              variant="body2"
+              component="div"
+            >
+              View all {comments.length} comments
+            </Typography>
+          </Link>
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              <Link to={`/${comment.user.username}`}>
+                <Typography
+                  variant="subtitle2"
+                  component="span"
+                  className={classes.commentUsername}
+                >
+                  {comment.user.username}
+                </Typography>{" "}
+                <Typography variant="body2" component="span">
+                  {comment.content}
+                </Typography>
+              </Link>
+            </div>
+          ))}
+          {/* TODO: REMOVE HARD CODED COMMENT DATE */}
+          <Typography color="textSecondary" className={classes.datePosted}>
+            5 DAYS AGO
+          </Typography>
+        </div>
+        <Hidden xsDown>
+          <Divider />
+          <Comment />
+        </Hidden>
+      </article>
+    </>
+  );
+}
+
+function LikeButton() {
+  return <>LIKE</>;
+}
+
+function SaveButton() {
+  return <>SAVE</>;
+}
+
+function Comment() {
+  return <>COMMENT</>;
 }
 
 export default FeedPost;
