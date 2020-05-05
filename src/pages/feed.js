f97b011b1cfcab39fe625058536f808f7e464f27
Charlie Astrada <carlosastrada@gmail.com>
2020-05-05 00:10:50 -0300

    Build skeleton feed with sidebar skeleton
    * uses data.js dummy data

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,10 +1,35 @@
 import React from "react";
 import { useFeedPageStyles } from "../styles";
+import Layout from "../components/shared/Layout";
+import UserCard from "../components/shared/UserCard";
+import FeedPost from "../components/feed/FeedPost";
+import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
+import { Hidden } from "@material-ui/core";
+
+import { getDefaultPost } from "../data";
 
 function FeedPage() {
-  useFeedPageStyles();
+  const classes = useFeedPageStyles();
 
-  return <div>FeedPage</div>;
+  return (
+    <Layout>
+      <div className={classes.container}>
+        <div>
+          {Array.from({ length: 5 }, () => getDefaultPost()).map((post) => (
+            <FeedPost key={post.id} post={post} />
+          ))}
+        </div>
+        <Hidden smDown>
+          <div className={classes.sidebarContainer}>
+            <div className={classes.sidebarWrapper}>
+              <UserCard />
+              <FeedSideSuggestions />
+            </div>
+          </div>
+        </Hidden>
+      </div>
+    </Layout>
+  );
 }
 
 export default FeedPage;
