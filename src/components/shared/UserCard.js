2fd67bfd1b4d77863eaa7d1af38a4f0305df8229
Charlie Astrada <carlosastrada@gmail.com>
2020-05-06 01:14:18 -0300

    Build UserCard component
    UserCard uses dummy data from data.js.

--- src/components/shared/UserCard.js
+++ src/components/shared/UserCard.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { useUserCardStyles } from "../../styles";
+import { Link } from "react-router-dom";
+import { Avatar, Typography } from "@material-ui/core";
+import { defaultUser } from "../../data";
 
-function UserCard() {
-  useUserCardStyles();
+function UserCard({ avatarSize = 44, user = defaultUser }) {
+  const classes = useUserCardStyles({ avatarSize });
+  const { profile_image, name, username } = user;
 
-  return <div>UserCard</div>;
+  return (
+    <div className={classes.wrapper}>
+      <Link to={`/${username}`}>
+        <Avatar
+          alt="user avatar"
+          className={classes.avatar}
+          src={profile_image}
+        />
+      </Link>
+      <div className={classes.nameWrapper}>
+        <Link to={`/${username}`}>
+          <Typography variant="subtitle2" className={classes.typography}>
+            {username}
+          </Typography>
+        </Link>
+        <Typography
+          className={classes.typography}
+          color="textSecondary"
+          variant="body2"
+        >
+          {name}
+        </Typography>
+      </div>
+    </div>
+  );
 }
 
 export default UserCard;
